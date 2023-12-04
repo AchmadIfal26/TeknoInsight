29815bfd83c330a0dcba9d5137d67b4d69848818
AchmadIfal26 <147014127+AchmadIfal26@users.noreply.github.com>
2023-12-04 11:56:50 +0700

    About Test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/style.css">
-    <title>TeknoInsight | About</title>
+    <title>TeknoInsight | Aboutt</title>
 </head>
 <body>
     <h1>Ini Halaman About</h1>
