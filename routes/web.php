6381412079d0acfd92646088531d201bdfbc66ac
AchmadIfal26 <147014127+AchmadIfal26@users.noreply.github.com>
2023-12-04 11:46:39 +0700

    Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Al Farazy",
+        "email" => "sidorukun0@gmail.com",
+        "image" => "ifal.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
