eae9e8a01bd5137e3f1485d4c4804bf2b8f213fd
AchmadIfal26 <147014127+AchmadIfal26@users.noreply.github.com>
2023-12-07 21:03:27 +0700

    Factory Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'Achmad Ifal',
-            'email' => 'achmadifal@gmail.com',
-            'password' => bcrypt('AchmadIfal')
-        ]);
+        // User::create([
+        //     'name' => 'Achmad Ifal',
+        //     'email' => 'achmadifal@gmail.com',
+        //     'password' => bcrypt('AchmadIfal')
+        // ]);
 
-        User::create([
-            'name' => 'Al Farazy',
-            'email' => 'alfarazy@gmail.com',
-            'password' => bcrypt('AlFarazy')
-        ]);
+        // User::create([
+        //     'name' => 'Al Farazy',
+        //     'email' => 'alfarazy@gmail.com',
+        //     'password' => bcrypt('AlFarazy')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -45,49 +46,8 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p>',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p>',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p>',
-            'category_id' => 3,
-            'user_id' => 1
-        ]);
-
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p>',
-            'category_id' => 1,
-            'user_id' => 2
-        ]);
-
-        Post::create([
-            'title' => 'Judul Kelima',
-            'slug' => 'judul-kelima',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ex ipsam sed quibusdam cum et hic, modi necessitatibus eligendi iste dolorem omnis ipsa officiis cumque aperiam? Reprehenderit, assumenda doloribus? Atque nihil laborum quidem culpa, perferendis aspernatur doloremque cum ipsum, libero totam, voluptatibus unde quae suscipit animi ducimus numquam molestiae tenetur modi sapiente aliquam distinctio. Modi velit fugit cumque! Labore veniam dolor, nam hic, unde inventore illo ad facilis similique maiores nulla alias quae, in delectus autem ducimus id quaerat modi molestiae harum ratione ipsum ea. Ipsa iure dolores omnis aspernatur voluptates obcaecati, adipisci, tempora impedit quae aperiam, labore inventore nobis.</p>',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
     }
 }
