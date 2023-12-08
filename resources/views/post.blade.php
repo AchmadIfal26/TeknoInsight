79e531e329142f6ee158fad4f8238ee902b5145d
AchmadIfal26 <147014127+AchmadIfal26@users.noreply.github.com>
2023-12-08 10:03:47 +0700

    N+1 Problem

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,8 @@
 @extends('layouts.main')
 @section('container')
     <article>
-        {{-- <h2>{{ $post->title }}</h2>
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        <h5>{{ $post->author }}</h5>
-        {!! $post->body !!} --}}
         <h1 class="mb-3">{{ $post->title }}</h1>
-        <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.main')
 @section('container')
-<h1 class="mb-3">Posts</h1>
+<h1 class="mb-3">{{ $title }}</h1>
 
     @foreach ($posts as $post)
         <article class="mb-3 border-bottom pb-4">
             <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+
             <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
+
             <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More...</a>
         </article>
     @endforeach
