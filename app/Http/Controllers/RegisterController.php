a19867bf67e52aa9c3683f9f638b37139b65ee68
AchmadIfal26 <147014127+AchmadIfal26@users.noreply.github.com>
2023-12-13 11:22:14 +0700

    User Login and Middleware

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,16 +16,16 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request) {
-        $validateData =  $request->validate([
+        $validateData = $request->validate([
             'name' => 'required|max:255',
             'username' => ['required', 'min:3', 'max:255', 'unique:users'],
             'email' => 'required|email:dns|unique:users',
             'password' => 'required|min:5|max:255'
         ]);
-        $validateData['password'] = Hash::make($validateData['password']);
 
+        $validateData['password'] = Hash::make($validateData['password']);
         User::create($validateData);
-        return redirect('/login')->with('success', 'Registration Successfull !!! Please Login');
+        return redirect('/login')->with('success', 'Registration Successful !!! Please login.');
 
     }
 }
